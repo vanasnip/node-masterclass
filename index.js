ce9177b5fcf23821a7b470dcf66e1a8d8130eea0
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-06 11:57:35 +0100

    Parsing Query Strings: retrieved query prop on parsedUrl, parsedUrl.query << relating to second param 'true' when parsedUrl is created.

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ var server = http.createServer(function(req,res){
   var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g,'');
   
+  // Get the query string as an object 
+  var queryStringObject = parsedUrl.query;
+
   // Get the HTTP Method
   var method = req.method.toLowerCase();
 
   //Send the response
   res.end('Hello World\n');
 
-
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath + ' with method ' + method);
+  console.log('Request received on path: ' + trimmedPath + ' with method ' + method + ' and with these qury string parameters: ' , queryStringObject);
 
 });
 
