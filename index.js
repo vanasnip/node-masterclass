4a59364c3c46238b4785cb69ab7801472588f9ad
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-06 11:17:57 +0100

    Initalised server with http and url modules that receive a request and parse the url

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+ * Primary file for the API
+ *
+ */
+
+//Dependencies 
+var http = require('http');
+var url = require('url');
+
+// the server should respond to all requests with a string
+var server = http.createServer(function(req,res){
+
+  //Get the url and parse it 
+  var parsedUrl = url.parse(req.url,true); 
+
+  //Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  
+
+  //Send the response
+  res.end('Hello World\n');
+
+
+  // Log the request path
+  console.log('Request received on path: ' + trimmedPath);
+
+});
+
+//Start the server, and have it listen on port 3000
+server.listen(3000,function(){
+  console.log('The server is listening on port 3000 now');
+});
