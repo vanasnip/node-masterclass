3d7fe258688b083b8db49d7c405924df3446eb34
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-09 13:37:07 +0100

    Routing Requests: Setup routing if prop on routing obj, defined some handlers when route is found and not found, used res.writeHead to send user status code

--- index.js
+++ index.js
@@ -33,13 +33,40 @@ var server = http.createServer(function(req,res){
   req.on('data', function(data){
     buffer += decoder.write(data);
   });
-  
+
   req.on('end', function(){
     buffer += decoder.end();
+    
+    // Choose the handler this request should go to, if one is not found use the not found handler
+    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound; 
 
-    //Send the response
-    res.end('Hello World\n');
+    //Construct the data object to send to the handler
+    var data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    };
 
+    // Router the request to the handler specified in the router
+    chosenHandler(data, function(statusCode, payload){
+      // Use the status cose called back by the handler or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+      // use the payload called back by the handler, or default to an empty function
+      payload  = typeof(payload) == 'object' ? payload : {};
+
+      // Convert the payload to a string
+      var payloadString = JSON.stringify(payload);
+
+      // Retrun the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log the request path
+      console.log('Returning this response: ', statusCode, payloadString);
+    });
     // Log the request path
     console.log('Request received with this payload:', buffer,'|headers:', headers);
   });
@@ -49,3 +76,19 @@ var server = http.createServer(function(req,res){
 server.listen(3000,function(){
   console.log('The server is listening on port 3000 now');
 });
+// Define the handlers
+var handlers = {};
+
+// Sample handler
+handlers.sample = function(data, callback){
+  // Callback a http status code, and a payload object
+  callback(406,{'name': 'sample handler'});
+};
+
+handlers.notFound = function(data, callback){
+  callback(404);
+}
+// Define a request router
+var router = {
+  'sample' : handlers.sample
+}
